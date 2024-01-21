0c70c2b22d57e60de77857b1e089d8e87d17bf2f
Dharshan J <117148127+DHARSHANJ@users.noreply.github.com>
2024-01-21 22:14:22 +0530

    Add files via upload

---
+++ marquee.php
@@ -0,0 +1,6 @@
+
+
+
+<marquee direction="left" behavior="scroll" onmousedown="this.stop()" onmouseover="this.stop()" onmousemove="this.stop()" onmouseout="this.start()" class="snipcss0-0-0-1 snipcss-jRcjB">
+    <h2>Hello Everyone. My name is Dharshan J. I'm a freelancher. I'm selling the websites at the minimum cost. For furthur details <a href="https://www.fiverr.com/s/x2daLQ" target="_blank">Click here</a>.</h2>
+</marquee>
